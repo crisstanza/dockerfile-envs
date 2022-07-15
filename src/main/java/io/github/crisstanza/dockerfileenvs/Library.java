e30246c57f603c6b6e3e0c0f61150f55bfb8893e
crisstanza <crisstanza@users.noreply.github.com>
2022-07-15 18:00:06 -0300

    Sort.

--- src/main/java/io/github/crisstanza/dockerfileenvs/Library.java
+++ src/main/java/io/github/crisstanza/dockerfileenvs/Library.java
@@ -3,7 +3,12 @@ package io.github.crisstanza.dockerfileenvs;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public final class Library
 {
@@ -15,20 +20,23 @@ public final class Library
 		final File inFile = new File(in);
 		final File outFile = new File(out);
 
-		final boolean append = false;
-		final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile, append));
-
+		final List<String> envs = new ArrayList<>();
 		final Scanner scanner = new Scanner(inFile);
 		while (scanner.hasNextLine()) {
 			final String line = scanner.nextLine();
 			if (line.startsWith("ENV ")) {
 				if (!line.contains("${")) {
 					final String parts[] = line.split("\\s+", 3);
-					writer.write(parts[1] + "=" + parts[2] + ";");
+					envs.add(parts[1] + "=" + parts[2]);
 				}
 			}
 		}
 
+		envs.sort(Comparator.comparing(String::toLowerCase));
+
+		final boolean append = false;
+		final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile, append));
+		writer.write(envs.stream().collect(Collectors.joining(";")));
 		writer.close();
 		return true;
 	}
--- src/main/java/io/github/crisstanza/dockerfileenvs/Main.java
+++ src/main/java/io/github/crisstanza/dockerfileenvs/Main.java
@@ -2,16 +2,10 @@ package io.github.crisstanza.dockerfileenvs;
 
 public final class Main
 {
-	private Library library = new Library();
-
 	public static final void main(final String[] args) throws Exception
 	{
-		new Main().start();
-	}
-
-	private final void start() throws Exception
-	{
-		System.out.println("Success? " + this.library.start());
+		final Library library = new Library();
+		System.out.println("Success? " + library.start());
 	}
 
 }
