dfbc0297b65401693c97e1648a5d8ceaa700d0a4
crisstanza <crisstanza@users.noreply.github.com>
2022-08-25 15:28:24 -0300

    Fix names with :; multiline output format.

--- src/main/java/io/github/crisstanza/dockerfileenvs/Main.java
+++ src/main/java/io/github/crisstanza/dockerfileenvs/Main.java
@@ -5,7 +5,7 @@ public final class Main {
     /**
      * Folder where is the Dockerfile.
      */
-    public static final String HOME = "C:\\_BITBUCKET.NEUTRINOS\\enforcement_request_service\\src\\main\\docker\\";
+    public static final String HOME = "C:\\tmp\\";
     public static final OutputMode OUTPUT_MODE = OutputMode.SINGLE_LINE;
 
     public static final void main(final String[] args) throws Exception {
